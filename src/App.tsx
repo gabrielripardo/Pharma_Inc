990b9989aae2896484425dcda512191da158aa5b
Gabriel Ripardo <gts.senna@gmail.com>
2021-12-14 00:25:39 -0300

    added base structure api fetch with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Home from "./pages/Home";
 import Header from "./pages/Header";
+import Test from "./pages/Test"
 import {Provider} from "react-redux"
-import {store} from "./store"
+// import {store} from "./store"
+
+import configureStore from "./store/configureStore"
+const store = configureStore();
 
 function App() {
   return (
     <Provider store={store}>      
       <Header/>
-      <Home/>
+      <Home/>      
     </Provider>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,16 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import api from "../services/api"
 import SimpleTable from "../components/SimpleTable"
 import { Container } from '@mui/material';
+import Patients from '../components/Patients';
 
-export default function Home() {
-    const dispatch = useDispatch()
-    const [patients, setPatients] = useState<any[]>([]);
-    
-    const ExtractItemsAction = () => (dispatch: any) => {      
-      dispatch ({type: "ITEMS_REQUESTED"});
-      const url = 'https://randomuser.me/api/?results=10';
-      fetch(url, {
-          method: "get"
-      })
-      .then(res => res.json())
-      .then(response => {
-          dispatch({type: "ITEMS_RECEIVED", items: response.data.results});
-          setPatients(response.data.results)
-      });
-      // api
-      // .get("/?results=10")
-      // .then((response) => { 
-      //       console.log(response.data.results); 
-      //       console.log("Type of: "+typeof(response.data.results))
-      //       const data = response.data.results
-
-      //       setPatients(data) 
-      //       dispatch({type: "ITEMS_RECEIVED", items: data});
-      //     })
-      // .catch((err) => {
-      //     console.error("ops! ocorreu um erro" + err);
-      // });
-    }
-
-    useEffect(() => {
-      ExtractItemsAction()
-    }, []);
+export default function Home() {    
 
     return (
-        <Container>
-            <h2>Dashboard</h2>
-            <SimpleTable />
-            {
-              patients.map(item => (
-                  <div>                    
-                    {item.name.first}
-                  </div>
-                )
-              )
-            }           
-
-            <button
-              onClick={ () =>
-                dispatch({type: 'INCREMENT'})
-              }
-            >
-              Adicionar 
-            </button> 
-        </Container>
+      <Container>
+        <h2>Dashboard</h2>
+        <SimpleTable />            
+        <Patients/>       
+      </Container>  
     )
 }

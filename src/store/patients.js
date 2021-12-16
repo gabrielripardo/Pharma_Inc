3b3ee4338e41b7d359d7960e021508c278c3aa46
Gabriel Ripardo <gts.senna@gmail.com>
2021-12-16 11:09:22 -0300

    when click loading more load news patients

---
+++ src/store/patients.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { apiCallBegan } from "./api";
+
+const slice = createSlice({
+    name: "patients",
+    initialState: {
+        list: [],
+        loading: false,
+    },
+
+    reducers: {
+        patientsRequested: (patients, action) => {
+            patients.loading = true;
+        },
+
+        patientsReceived: (patients, action) => {
+            console.log("## patients ")
+            console.log(patients)
+            patients.list = action.payload;
+            patients.loading = false;
+        },
+
+        patientsRequestFailed: (patients, action) => {
+            patients.loading = false;
+        },
+    },
+});
+
+export default slice.reducer;
+
+const { patientsRequested, patientsReceived, patientsRequestFailed } = slice.actions;
+
+// const url = `/?page=1&results=10`;
+
+export const loadpatients = (page) => (dispatch) => {
+    return dispatch(
+        apiCallBegan({
+            url: `/?page=${page}&results=10`,
+            onStart: patientsRequested.type,
+            onSuccess: patientsReceived.type,
+            onError: patientsRequestFailed.type,
+        })
+    );
+};

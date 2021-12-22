fe19544bef42be083c608988ce490a3330ca9c21
Gabriel Ripardo <gts.senna@gmail.com>
2021-12-22 20:35:55 -0300

    added modal with all datas in tags

--- src/components/DetailsModal.tsx
+++ src/components/DetailsModal.tsx
@@ -4,6 +4,7 @@ import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Patient from '../models/Patient.model'
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -18,31 +19,38 @@ const style = {
 };
 
 export default function DetailsModal(props: any) {
-  console.log(props.data)
+  const {data, open, handleClose} = props
+  console.log(data)
 
   return (
     <div>      
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
-        open={props.open}
-        onClose={props.handleClose}
+        open={open}
+        onClose={handleClose}
         closeAfterTransition
         BackdropComponent={Backdrop}
         BackdropProps={{
           timeout: 500,
         }}
       >
-        <Fade in={props.open}>
+        <Fade in={open}>
           <Box sx={style}>
             <Typography id="transition-modal-title" variant="h6" component="h2">
               Detalhes do paciente
             </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              {props.data.gender}
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-              {/* {props.data.id.name} */}
-              <img src={props.data.picture.medium}></img>
+            <Typography id="transition-modal-description" component={'span'} sx={{ mt: 2 }}>              
+              <img src={data.picture.medium}></img>
+              <p>Nome: <span>{`${data.name.title} ${data.name.first} ${data.name.last}`}</span></p>                           
+              <p>Email: <span>{data.email}</span> </p>
+              <p>Gênero: <span>{data.gender}</span></p>
+              <p>Data de nascimento: <span>{data.dob.date}</span></p>
+              <p>Telefone: <span>{data.phone}</span> </p>
+              <p>Nacionalidade: <span>{data.location.country}</span> </p>
+              <p>Endereço: <span>{`${data.location.state} - ${data.location.city} - ${data.location.street.name} - ${data.location.street.number}`}</span> </p>
+              <p>ID (Número de identificação): <span>{data.id.value}</span></p>
+              <p>URL para compartilhamento: </p>
             </Typography>
           </Box>
         </Fade>
--- src/models/Patient.model.ts
+++ src/models/Patient.model.ts
@@ -1,6 +1,19 @@
 export default interface Patient {
+    id: {
+        value: String,
+        name: String,
+    }
     name: String;
     email: String;
     gender: String;
     picture: {medium: String, large: String};  
+    dob: {date: String};
+    phone: String;
+    location: {
+        country: String, 
+        state: String, 
+        city: String,
+        street: {name: String, number: String} 
+    };    
 }
+
